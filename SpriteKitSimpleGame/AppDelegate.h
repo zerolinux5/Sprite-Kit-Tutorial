6124bd1658368a91a8609aabae2c599764953a59
Jesus Magana <maganazero5@gmail.com>
2014-06-24 22:51:38 -0700

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Jesus Magana on 6/24/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitSimpleGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpriteKitSimpleGame
+//
+//  Created by Jesus Magana on 6/24/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
